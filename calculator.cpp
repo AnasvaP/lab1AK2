2f0b1e2bb47e5914d79c2b0e0b04c264b8fec049
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:26:35 +0200

    2 commits combined formatting: use tabs instead of spaces
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    fix truncation error
    
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,15 @@
 
 int Calculator::Add (double a, double b)
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
 	return a + b + 0.5;
+=======
+	return a + b;
+>>>>>>> cbcb06d... formatting: use tabs instead of spaces
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3... fix truncation error
 }
 
 int Calculator::Sub (double a, double b)
